19ae1b38859d4352dc3ac503dc73a41a7bd3d8fa
s3lfcode <aeti0sxd@gmail.com>
2023-03-21 11:10:43 +0300

    refactor: removing unnecessary code

--- HaHdevSchool/Utils/ViewController.swift
+++ HaHdevSchool/Utils/ViewController.swift
@@ -102,12 +102,6 @@ class ViewController: UIViewController {
     lazy var phoneTextField: MaterialTextField = {
         let textField = MaterialTextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
-//        textField.font = .systemFont(ofSize: 25, weight: .medium)
-//        textField.textColor = .black
-//        textField.attributedPlaceholder = .init(string: "  Телефон", attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 190/255, green: 179/255, blue: 167/255, alpha: 0.5)])
-//        textField.layer.masksToBounds = true
-//        textField.layer.cornerRadius = Constants.cornerRadius
-        //textField.delegate = self
         return textField
     }()
     
@@ -166,61 +160,3 @@ class ViewController: UIViewController {
 
     }
 }
-
-//extension ViewController: UITextFieldDelegate {
-//
-//    private func phoneFormatter(mask: String, number: String) -> String {
-//        let onlyNumbers = number.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
-//        var result = ""
-//        var index = onlyNumbers.startIndex
-//        for char in mask where index < onlyNumbers.endIndex {
-//            if char == "X" {
-//                result.append(onlyNumbers[index])
-//                index = onlyNumbers.index(after: index)
-//            } else {
-//                result.append(char)
-//            }
-//        }
-//        return result
-//    }
-//
-//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-//        guard let text = textField.text else {return false}
-//
-//        let newString = (text as NSString).replacingCharacters(in: range, with: string)
-//
-//        let mask = "+X (XXX) XXX-XX-XX"
-//        if newString.count == mask.count+1 { return false }
-//
-//        textField.text = phoneFormatter(
-//            mask: mask,
-//            number: newString)
-//
-//        if string.count == 0 {
-//            guard let deletePosition = textField.position(
-//                from: textField.beginningOfDocument,
-//                offset: range.location) else { return false }
-//
-//            textField.selectedTextRange = textField.textRange(from: deletePosition, to: deletePosition)
-//        } else {
-//            guard let finalText = textField.text else { return false }
-//
-//            let charArray = Array(finalText)
-//            var location = 0
-//            for index in range.location+range.length..<charArray.count {
-//                if charArray[index].isNumber{
-//                    location = index+1
-//                    break
-//                }
-//            }
-//
-//            guard let insertPosition = textField.position(
-//                from: textField.beginningOfDocument,
-//                offset: location) else { return false }
-//
-//            textField.selectedTextRange = textField.textRange(from: insertPosition, to: insertPosition)
-//        }
-//
-//        return false
-//    }
-//}
